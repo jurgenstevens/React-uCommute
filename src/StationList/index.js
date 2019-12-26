c5ced9f1664ccb477783cd87b8bde1ca8cea7725
Jurgen <jurgen.stevens@yahoo.com>
2019-12-26 00:35:04 -0600

    All stations added to db and select buttons styled

--- src/StationList/index.js
+++ src/StationList/index.js
@@ -149,12 +149,18 @@ class StationList extends React.Component {
 							{lines}
 						</select>
 						<h3>Direction:</h3>
-						<select name="DirectionOrigin" className="direction" onChange={this.handleChange} value={this.state.DirectionOrigin}>
+						<select name="DirectionOrigin" 
+								className="direction" 
+								onChange={this.handleChange} 
+								value={this.state.DirectionOrigin}>
 							<option>---</option>
 							{direction}
 						</select>
 						<h3>Select Station:</h3>
-						<select name="originStation" className="stationList" onChange={this.handleChange} value={this.state.originStation}>
+						<select name="originStation" 
+								className="stationList" 
+								onChange={this.handleChange} 
+								value={this.state.originStation}>
 							<option>---</option>
 							{ originStations }
 						</select>
@@ -203,85 +209,3 @@ class StationList extends React.Component {
 
 
 export default StationList
-
-/*
-
-	<select onChange={handle choose color}>
-		<option value="Orange"></option>
-		<option value="Red"></option>
-		<option value="Blue"></option>
-		<option value="Green"></option>
-		<option value="Pink"></option>
-		<option value="Brown"></option>
-		<option value="Purple"></option>
-		<option value="Yellow"></option>
-	</select>
-
-	<select onChange={handle choose station} >
-		<option value=""></option>
-	</select>
-
-*/
-
-
-
-
-
-// import React from 'react';
-// import { Card, Button, Dropdown } from 'semantic-ui-react';
-
-// function StationList() {
-// 	const lineColors = ["Orange", "Red", "Blue", "Blue", "Green", "Pink", "Brown", "Purple", "Yellow"]
-// 	const line = lineColors.map((number) =>
-// 		<option>{line}</option>
-// 	);
-// // 	// create an array of the unique colors -- use a loop
-
-// // 	// f that looks in state and creates a new list of stations based on whatever color is chosen in state
-
-// // 	// or if no color is chosen, grey out the drop down
-
-// return (
-// 	<Card.Group>
-// 		<h1>Origin</h1>
-// 		<h3>Select Line:</h3>	
-// 		<select className="selectLine">
-// 			<option>{line}</option>
-// 		</select>
-//  		<h3>Select Station:</h3>
-//  		<select className="selectStation">
-
-// 		</select>
-// 		<h1>Destination</h1>
-//  		<h3>Select Line:</h3>
-//  		<select className="selectLine">
-
-//  		</select>
-//  		<h3>Select Station:</h3>
-//  		<select className="selectStation">
-
-//  		</select>
-//  	</Card.Group>
-//  	)
-// }
-
-// export default StationList
-
-// // /*
-
-// // 	<select onChange={handle choose color}>
-// // 		<option value="Orange"></option>
-// // 		<option value="Red"></option>
-// // 		<option value="Blue"></option>
-// // 		<option value="Green"></option>
-// // 		<option value="Pink"></option>
-// // 		<option value="Brown"></option>
-// // 		<option value="Purple"></option>
-// // 		<option value="Yellow"></option>
-// // 	</select>
-
-// // 	<select onChange={handle choose station} >
-// // 		<option value=""></option>
-// // 	</select>
-
-// // */
